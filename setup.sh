cd0b5707f203a8d4e5da382236c0257065fb0ffe
Jon Stokes <jon.m.stokes@gmail.com>
2017-10-23 09:18:47 -0500

    Added zshrc and updated setup script

--- setup.sh
+++ setup.sh
@@ -15,11 +15,13 @@ brew install node yarn watchman
 # ZSH and oh-my-zsh
 brew install zsh zsh-completions
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+mkdir ~/.oh-my-zsh/custom/themes
+cp robbyrussell.zsh-theme ~/.oh-my-zsh/custom/themes
+cp .zshrc ~
 
-# RVM and ruby
+# RVM and ruby dev stuff
+brew install phantomjs chromedriver
 brew install gnupg
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
 
 \curl -sSL https://get.rvm.io | bash
-
-brew install phantomjs chromedriver
