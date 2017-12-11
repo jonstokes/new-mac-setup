cd6b3a71ac2171fbb0cb9c5038eebfc153bca9ec
Jon Stokes <jon.m.stokes@gmail.com>
2017-12-11 12:38:52 -0600

    Copy over ackrc

--- setup.sh
+++ setup.sh
@@ -32,4 +32,6 @@ brew install zsh zsh-completions
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 mkdir ~/.oh-my-zsh/custom/themes
 cp robbyrussell.zsh-theme ~/.oh-my-zsh/custom/themes
+
 cp .zshrc ~
+cp. ackrc ~
