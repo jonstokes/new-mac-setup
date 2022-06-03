5751304e6f95378e863d9ff26e1524d0d125d275
Jon Stokes <jon@jonstokes.com>
2022-06-03 09:26:15 -0500

    Erlang plugin

--- setup.sh
+++ setup.sh
@@ -29,6 +29,7 @@ brew services start postgresql
 # Elixir and Go
 brew install gpg gawk
 brew install asdf
+asdf plugin add erlang https://github.com/asdf-vm/asdf-erlang.git
 asdf plugin-add elixir https://github.com/asdf-vm/asdf-elixir.git
 asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
 
